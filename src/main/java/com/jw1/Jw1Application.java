3da0f6134755aea8d755ca21ca5afe0337ea28cf
Rod Johnson <johnsonroda@gmail.com>
2019-01-17 15:22:43 -0800

    Initial commit from Atomist

---
+++ src/main/java/com/jw1/Jw1Application.java
@@ -0,0 +1,12 @@
+package com.jw1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jw1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jw1Application.class, args);
+	}
+}
---
+++ src/main/java/com/jw1/Jw1Controller.java
@@ -0,0 +1,20 @@
+package com.jw1;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+class Jw1Controller {
+
+    @GetMapping("/hello/{name}")
+    public String person(@PathVariable String name) {
+        return "Hello " + name + "!";
+    }
+
+    @GetMapping("/")
+    public String root() {
+        return "Hello, Delta Blues! Add /hello/there to the URL to get a friendly reply.";
+    }
+
+}
